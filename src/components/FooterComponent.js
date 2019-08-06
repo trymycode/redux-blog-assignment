95c38258f2d6cc710531cb233ae35d47d6a4ee0f
Imita Singha <imita@psqad.com>
2019-08-06 15:52:34 +0530

    changed footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -2,26 +2,87 @@ import React, { Component } from "react";
 import "./FooterComponent.css";
 import { Link } from "react-router-dom";
 class FooterComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeHome: true,
+      activeCreate: false,
+      activePopular: false
+    };
+  }
+  handleClick = e => {
+    if(e.target.getAttribute('value') == 'activeHome'){
+      console.log(e.target.getAttribute('value'))
+      this.setState({
+        activeHome: ! this.state.activeHome,
+        activeCreate: false,
+        activePopular: false
+      })
+    } else if(e.target.getAttribute('value') == 'activeCreate'){
+      console.log(e.target.getAttribute('value'))
+      this.setState({
+        activeHome: false,
+        activeCreate: ! this.state.activeCreate,
+        activePopular: false
+      })
+    }
+    else {
+      console.log(e.target.getAttribute('value'))
+      this.setState({
+        activeHome: false,
+        activePopular: ! this.state.activePopular,
+        activeCreate: false
+        
+      })
+    }
+  };
   render() {
     return (
-       <div className="footer d-flex justify-content-around pt-2">
+      <div className="footer d-flex justify-content-around pt-2">
         <Link to="/">
-          <label className="center ml-1"  title="Home Page">
-            <i className="fa fa-2x fa-home text-secondary" />
+          <label
+            className="center ml-1"
+            title="Home Page"
+            onClick={this.handleClick}
+          >
+            <i
+              className={
+                this.state.activeHome
+                  ? "fa fa-2x fa-home text-info"
+                  : "fa fa-2x fa-home text-secondary"
+              }
+              value="activeHome"
+            />
           </label>
         </Link>
-        <Link to="/popular" >
+        <Link to="/popular">
           <label className="center ml-1 popular" title="Popular Blogs Page">
-            <i className="fa fa-2x fa-heart text-secondary"/>
+            <i
+              className={
+                this.state.activePopular
+                  ? "fa fa-2x fa-heart text-info"
+                  : "fa fa-2x fa-heart text-secondary"
+              }
+              value="activePopular"
+              onClick={this.handleClick}
+            />
           </label>
         </Link>
         <Link to="/create">
           <label className="center ml-1" title="Create Blog Page">
-            <i className="fa fa-2x fa-plus text-secondary" />
+            <i
+              className={
+                this.state.activeCreate
+                  ? "fa fa-2x fa-plus text-info"
+                  : "fa fa-2x fa-plus text-secondary"
+              }
+              value="activeCreate"
+              onClick={this.handleClick}
+            />
           </label>
         </Link>
       </div>
-     
     );
   }
 }
--- src/components/PopularComponent.js
+++ src/components/PopularComponent.js
@@ -1,43 +1,43 @@
 import React, { Component } from "react";
 import BlogComponent from "./BlogComponent";
 import "./PopularComponent.css";
-import PageLoader from './PageLoader';
+import PageLoader from "./PageLoader";
 
 function PopularComponent(props) {
   const compare = (a, b) => {
-        const genreA = a.likes;
-        const genreB = b.likes;
-    
-        let comparison = 0;
-        if (genreA > genreB) {
-          comparison = 1;
-        } else if (genreA < genreB) {
-          comparison = -1;
-        }
-        return comparison * -1;
-      };
-      let descendingBlogs  = null;
-     descendingBlogs = props.blogs.sort(compare);
-     
+    const genreA = a.likes;
+    const genreB = b.likes;
+
+    let comparison = 0;
+    if (genreA > genreB) {
+      comparison = 1;
+    } else if (genreA < genreB) {
+      comparison = -1;
+    }
+    return comparison * -1;
+  };
+  let descendingBlogs = null;
+  descendingBlogs = props.blogs.sort(compare);
+
   return (
-      <div>
-         <div className="main-page">
-         <nav className="navbar navbar-dark bg-dark text-light topNav">
-            
-            <a class="navbar-brand title">Popular Blogs</a>
-            <form class="form-inline title">
-              <input 
-              class="form-control mr-sm-2" 
-              type="search" 
+    <div>
+      <div className="main-page">
+        <nav className="navbar navbar-dark bg-dark text-light topNav">
+          <a className="navbar-brand title">Popular Blogs</a>
+          <form className="form-inline title">
+            <input
+              className="form-control mr-sm-2"
+              type="search"
               placeholder="Search by title or author name"
               aria-label="Search"
               // onChange={this.handleChange}
-              style={{width:"300px", textAlign:"center"}}/>
-            </form>
-        
+              style={{ width: "300px", textAlign: "center" }}
+            />
+          </form>
         </nav>
-          <div className="container">
-          { descendingBlogs!= null?  <div className="blogs">
+        <div className="container">
+          {descendingBlogs != null ? (
+            <div className="blogs">
               {descendingBlogs.map(blog => (
                 <BlogComponent
                   data={{
@@ -53,13 +53,13 @@ function PopularComponent(props) {
                   key={blog.id}
                 />
               ))}
-            </div>: <PageLoader/>}
-           
-          </div>
-  
+            </div>
+          ) : (
+            <PageLoader />
+          )}
         </div>
       </div>
-      
-  )
+    </div>
+  );
 }
 export default PopularComponent;
