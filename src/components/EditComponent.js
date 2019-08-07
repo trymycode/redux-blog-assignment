bc02a38e487d891c71a711fc41b2b3334fca87e9
Imita Singha <imita@psqad.com>
2019-08-07 14:24:37 +0530

    changed edit component

--- src/components/EditComponent.js
+++ src/components/EditComponent.js
@@ -4,47 +4,49 @@ import PropTypes from "prop-types";
 import { editPost } from "../actions/postActions";
 import { withRouter } from "react-router-dom";
 import axios from "axios";
+import NotFoundComponent from "./NotFoundComponent";
 class EditComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      blogDetails: null,
-      title: "",
-      author: "",
-      description: "",
+      NotFound: false,
+      title: null,
+      author: null,
+      description: null,
       tags: null,
-      image: "",
+      image: null,
       published: true
     };
   }
-
-  // blogDetails = this.props.blogs.filter(
-  //   blog => blog.id === Number(this.props.match.params.id)
-  //   );
-  // items.forEach(function(item){
-  //   copy.push(item*item);
-  // });
   componentDidMount() {
-    const id = parseInt(this.props.match.params.id, 10);
+    const id = Number(this.props.match.params.id);
+    console.log("id", id);
     let filteredBlog = null;
+    if (!id) {
+     this.setState({
+       NotFound: true
+     })
+  }else{
     this.props.blogs.forEach(function(blog) {
       if (blog.id === id) {
         filteredBlog = blog;
       } else {
-        // console.log("checking");
+
       }
     });
-    if (filteredBlog != null) {
-      this.setState({
-        title: filteredBlog.title,
-        author: filteredBlog.author,
-        description: filteredBlog.description,
-        tags: filteredBlog.tags,
-        image: filteredBlog.image
-      });
-    }
+  
+  if (filteredBlog != null) {
+    this.setState({
+      NotFound: false,
+      title: filteredBlog.title,
+      author: filteredBlog.author,
+      description: filteredBlog.description,
+      tags: filteredBlog.tags,
+      image: filteredBlog.image
+    });
   }
-
+  }
+  }
   onSubmit = e => {
     e.preventDefault();
     console.log("onsubmit is called");
@@ -91,94 +93,105 @@ class EditComponent extends Component {
     });
   };
   render() {
-    return (
-      <div>
-        <div className="position">Edit</div>
-        <div className="container">
-          <form onSubmit={this.onSubmit} className="text-center">
-            <div className="form-group row">
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  className="form-control rounded border border-secondary"
-                  name="title"
-                  placeholder="Title"
-                  value={this.state.title}
-                  onChange={this.onFieldChange}
-                  maxLength="40"
-                  title="Must be within 40 characters"
-                  required
-                />
+    console.log(this.state.title);
+    console.log("this.state.notfound",this.state.NotFound )
+    if (!this.state.NotFound) {
+      return (
+        <div>
+          <div className="position">Edit</div>
+          <div className="container">
+            <form onSubmit={this.onSubmit} className="text-center">
+              <div className="form-group row">
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    className="form-control rounded border border-secondary"
+                    name="title"
+                    placeholder="Title"
+                    value={this.state.title}
+                    onChange={this.onFieldChange}
+                    maxLength="40"
+                    title="Must be within 40 characters"
+                    required
+                  />
+                </div>
               </div>
-            </div>
-            <div className="form-group row">
-              <div className="col-sm-10">
-                <textarea
-                  className="form-control rounded border border-secondary"
-                  rows="3"
-                  name="description"
-                  placeholder="Description"
-                  value={this.state.description}
-                  onChange={this.onFieldChange}
-                  maxLength="500"
-                  title="Must be within 500 characters"
-                  required
-                />
+              <div className="form-group row">
+                <div className="col-sm-10">
+                  <textarea
+                    className="form-control rounded border border-secondary"
+                    rows="3"
+                    name="description"
+                    placeholder="Description"
+                    value={this.state.description}
+                    onChange={this.onFieldChange}
+                    maxLength="500"
+                    title="Must be within 500 characters"
+                    required
+                  />
+                </div>
               </div>
-            </div>
-            <div className="form-group row">
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  className="form-control rounded border border-secondary"
-                  name="tags"
-                  placeholder="Category/Tags"
-                  value={this.state.tags}
-                  onChange={this.onFieldChange}
-                  maxLength="30"
-                  title="Must be within 40 characters"
-                  required
-                />
+              <div className="form-group row">
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    className="form-control rounded border border-secondary"
+                    name="tags"
+                    placeholder="Category/Tags"
+                    value={this.state.tags}
+                    onChange={this.onFieldChange}
+                    maxLength="30"
+                    title="Must be within 40 characters"
+                    required
+                  />
+                </div>
               </div>
-            </div>
-            <div className="form-group row">
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  className="form-control rounded border border-secondary"
-                  name="author"
-                  placeholder="Author"
-                  value={this.state.author}
-                  onChange={this.onFieldChange}
-                  maxLength="30"
-                  title="Must be within 30 characters"
-                  required
-                />
+              <div className="form-group row">
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    className="form-control rounded border border-secondary"
+                    name="author"
+                    placeholder="Author"
+                    value={this.state.author}
+                    onChange={this.onFieldChange}
+                    maxLength="30"
+                    title="Must be within 30 characters"
+                    required
+                  />
+                </div>
               </div>
-            </div>
-            <div className="form-group row">
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  className="form-control rounded border border-secondary"
-                  name="image"
-                  placeholder="Image URL Only"
-                  value={this.state.image}
-                  onChange={this.onFieldChange}
-                  title="Must be an Image URL"
-                  required
-                />
+              <div className="form-group row">
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    className="form-control rounded border border-secondary"
+                    name="image"
+                    placeholder="Image URL Only"
+                    value={this.state.image}
+                    onChange={this.onFieldChange}
+                    title="Must be an Image URL"
+                    required
+                  />
+                </div>
               </div>
-            </div>
-            <input
-              type="submit"
-              value="CHANGE"
-              className="btn btn-info col-sm-10 text-center"
-            />
-          </form>
+              <input
+                type="submit"
+                value="CHANGE"
+                className="btn btn-info col-sm-10 text-center"
+              />
+            </form>
+          </div>
         </div>
-      </div>
-    );
+      );
+     
+    } else {
+      return (
+        <div>
+          <NotFoundComponent />
+        </div>
+      );
+    }
   }
 }
 EditComponent.propTypes = {
