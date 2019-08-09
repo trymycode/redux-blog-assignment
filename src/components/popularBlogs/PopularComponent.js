d1c5308d59454940f8d86534e36a79c5654ca604
Imita Singha <imita@psqad.com>
2019-08-09 17:56:29 +0530

    Create separate searchbar component and included it into popular component

--- src/components/popularBlogs/PopularComponent.js
+++ src/components/popularBlogs/PopularComponent.js
@@ -2,9 +2,18 @@ import React, { Component } from "react";
 import BlogComponent from "../blogs/BlogComponent";
 import "./PopularComponent.css";
 import PageLoader from "../pageLoader/PageLoader";
+import SearchbarComponent from "../searchbar/SearchbarComponent";
 
-function PopularComponent(props) {
-  const compare = (a, b) => {
+class PopularComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      descendingBlogs: []
+    };
+  }
+
+  compare = (a, b) => {
     const genreA = a.likes;
     const genreB = b.likes;
 
@@ -16,48 +25,54 @@ function PopularComponent(props) {
     }
     return comparison * -1;
   };
-  let descendingBlogs = null;
-  descendingBlogs = props.blogs.sort(compare);
 
-  return (
-    <div>
-      <div className="main-page">
-        <nav className="navbar navbar-dark bg-dark text-light topNav">
-          <a className="navbar-brand title">Popular Blogs</a>
-        
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search by title or author name"
-              aria-label="Search"
-              style={{ width: "300px", textAlign: "center" }}
-            />
-        </nav>
-        <div className="container">
-          {descendingBlogs != null ? (
-            <div className="blogs">
-              {descendingBlogs.map(blog => (
-                <BlogComponent
-                  data={{
-                    id: blog.id,
-                    author: blog.author,
-                    date: blog.created_at,
-                    image: blog.image,
-                    title: blog.title,
-                    likes: blog.likes,
-                    description: blog.description,
-                    tags: blog.tags
-                  }}
-                  key={blog.id}
-                />
-              ))}
-            </div>
-          ) : (
-            <PageLoader />
-          )}
+  componentWillMount() {
+    this.setState({
+      descendingBlogs: this.props.blogs.sort(this.compare)
+    });
+  }
+  // getting searched blog details from searchbar component
+  updateBlogsOnSearch=(searchedBlogs)=>{
+      console.log("searchedBlogs", searchedBlogs);
+      this.setState({
+        descendingBlogs: searchedBlogs
+      })
+  }
+  render() {
+    return (
+      <div>
+        <div className="main-page">
+          <nav className="navbar navbar-dark bg-dark text-light topNav">
+            <a className="navbar-brand title">Popular Blogs</a>
+            <SearchbarComponent blogs={this.state.descendingBlogs} updateBlogsOnSearch={this.updateBlogsOnSearch}/>
+          </nav>
+          <div className="container">
+            {this.state.descendingBlogs != null ? (
+              <div className="blogs">
+                {this.state.descendingBlogs.map(blog => (
+                  <BlogComponent
+                    data={{
+                      id: blog.id,
+                      author: blog.author,
+                      date: blog.created_at,
+                      image: blog.image,
+                      title: blog.title,
+                      likes: blog.likes,
+                      description: blog.description,
+                      tags: blog.tags
+                    }}
+                    key={blog.id}
+                  />
+                ))}
+              </div>
+            ) : (
+              <PageLoader />
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
 export default PopularComponent;
--- src/components/searchbar/searchbarComponent.js
+++ src/components/searchbar/searchbarComponent.js
@@ -5,13 +5,21 @@ class SearchbarComponent extends Component {
     super(props);
 
     this.state = {
-      searchText: "",
+      currentBlogs: [],
+      display: false,
       filtered: [],
-      display: false
+      searchText: ""
     };
+    this.handleClick = this.handleClick.bind(this);
   }
-  handleClick = e => {
-    let filteredNewArray = this.state.filtered.filter(
+  componentWillMount() {
+    this.setState({
+      currentBlogs: this.props.blogs
+    });
+  }
+
+  handleClick(e) {
+    let filteredNewArray = this.state.currentBlogs.filter(
       blog =>
         blog.title.toLowerCase().indexOf(e.target.innerText.toLowerCase()) !==
           -1 ||
@@ -23,16 +31,21 @@ class SearchbarComponent extends Component {
       filtered: filteredNewArray,
       display: false
     });
-  };
-  handleChange(e) {
+    this.props.updateBlogsOnSearch(filteredNewArray);
+  }
+
+  handleChange = e => {
     console.log("e -", e.target.value);
+    let filteredNewArray = this.props.blogs;
     if (e.target.value.length > 0) {
-      console.log(this.props.blogs);
-      let filteredNewArray = this.props.blogs.filter(
+     filteredNewArray = this.state.currentBlogs.filter(
         blog =>
-          blog.title.toLowerCase().indexOf(e.target.value.toLowerCase()) !==
-            -1 ||
-          blog.author.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1
+          blog.title
+            .toLowerCase()
+            .indexOf(this.state.searchText.toLowerCase()) !== -1 ||
+          blog.author
+            .toLowerCase()
+            .indexOf(this.state.searchText.toLowerCase()) !== -1
       );
       this.setState({
         searchText: e.target.value,
@@ -42,57 +55,52 @@ class SearchbarComponent extends Component {
     } else {
       this.setState({
         searchText: "",
-        filtered: this.props.blogs,
+        filtered: this.state.currentBlogs,
         display: false
       });
     }
-  }
-
+    this.props.updateBlogsOnSearch(filteredNewArray);
+  };
   render() {
-    console.log(this.props.blogs);
-    if (this.props.blogs.length !== 0) {
-      return (
-        <div>
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search by title or author name"
-            aria-label="Search"
-            style={{ width: "300px", textAlign: "center" }}
-            onChange={this.handleChange}
-          />
-          {this.state.display ? (
-            <div>
-              <ul
-                className="list-group"
-                style={{
-                  position: "fixed",
-                  top: "10%",
-                  left: "75%",
-                  zIndex: "1000",
-                  maxHeight: "300px",
-                  overflow: "auto"
-                }}
-              >
-                {this.state.filtered.map(blog => (
-                  <li
-                    className="list-group-item list-group-item-light list-group-item-action"
-                    onClick={this.handleClick}
-                    key={blog.id}
-                    value={blog.title}
-                    type="button"
-                  >
-                    {blog.title}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ) : null}
-        </div>
-      );
-    } else {
-      return <p>loading</p>;
-    }
+    return (
+      <div>
+        <input
+          className="form-control mr-sm-2"
+          placeholder="Search by title or author name"
+          style={{ width: "300px", textAlign: "center" }}
+          onChange={this.handleChange}
+          value={this.state.searchText}
+        />
+
+        {this.state.display ? (
+          <div>
+            <ul
+              className="list-group"
+              style={{
+                position: "fixed",
+                top: "10%",
+                left: "75%",
+                zIndex: "1000",
+                maxHeight: "300px",
+                overflow: "auto"
+              }}
+            >
+              {this.state.filtered.map(blog => (
+                <li
+                  className="list-group-item list-group-item-light list-group-item-action"
+                  onClick={this.handleClick}
+                  key={blog.id}
+                  value={blog.title}
+                  type="button"
+                >
+                  {blog.title}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
+      </div>
+    );
   }
 }
 export default SearchbarComponent;
